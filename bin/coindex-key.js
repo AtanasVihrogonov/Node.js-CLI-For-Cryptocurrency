409dad2117d9989d33c4008e43ac8a87c8698317
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-08-04 19:57:28 +0100

    add .gitignore

---
+++ bin/coindex-key.js
@@ -0,0 +1,9 @@
+const program = require('commander');
+
+program
+    .command('set')
+    .description('Set API Key -- Get at https://nomics.com')
+    .action(() => console.log('Hello from set'));
+
+program
+    .parse(process.argv);
