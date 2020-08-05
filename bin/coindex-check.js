b7cd91876a3e6fb9f1f9f85b3f8c560a19ddda9f
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-08-05 12:46:21 +0100

    set a custom error

--- bin/coindex-check.js
+++ bin/coindex-check.js
@@ -4,7 +4,11 @@ const check = require('../commands/check');
 program
     .command('price')
     .description('Check price of coins')
-    .option('--coin <type>', 'Add specific coin types and CSV format', 'BTC,ETH, XRP')
+    .option(
+        '--coin <type>',
+        'Add specific coin types and CSV format',
+        'BTC,ETH, XRP'
+    )
     .option('--cur <currency>', 'Change the currency', 'GBP')
     .action((cmd) => check.price(cmd));
 
